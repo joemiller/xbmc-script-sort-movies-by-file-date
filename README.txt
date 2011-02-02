c884f0bfd6dd5db9e42993ffaffc809fb9fe2c08
joe miller <joeym@joeym.net>
2011-02-01 17:03:22 -0700

    many changes, see 1.1 in changelog.txt

--- README.txt
+++ README.txt
@@ -1,15 +1,15 @@
 Purpose
 -------
 
-This script will "fix" the "sort-by: Date Added" feature in your Movie Library.  It does this
-by looking at the file creation time of all of the movies in your library and re-sorting the
-movie database using these timestamps.
+This script will re-sort your Movie Library by looking at the timestamps
+on your Movies.
 
-The main use case for this would be after you have to reset your XBMC Library and all of
-your movies are now sorted alphabetically instead of by the date you added the files
-to your library.
+The main use case for this would be to run this after you reset your XBMC
+Library and all of your movies are now sorted alphabetically instead of by
+the date you added the files to your library.
 
-This was inspired by my own personal needs, and apparantly many others on this thread:
+This was inspired by my own personal needs, and apparantly many others
+on this thread:
 http://forum.xbmc.org/showthread.php?t=58525
 
 How to install and run
@@ -29,33 +29,36 @@ How to install and run
 
 How it works
 ------------
-First, some background:  The XBMC Movie Library is contained in a SQLite database with each
-movie contained in a row in the 'movie' table.  There is no timestamp in the database
-indicating "Date Added", instead, XBMC simply relies on the order of the rows in the DB.
-This is why sorting by Date Added looks more like sorting by alphabetical order when you
+First, some background:  The XBMC Movie Library is contained in a SQLite
+database with each movie contained in a row in the 'movie' table.  There is
+no timestamp in the database indicating "Date Added", instead, XBMC simply
+relies on the order of the rows in the DB.  This is why sorting by
+Date Added looks more like sorting by alphabetical order when you
 reset your movie library.
 
-This script changes the order of the movies by modifiying the ID ('idMovie') column in
-the 'movies' table to be in the same order as the creation-timestamps of the movie 
-files themselves.  The 'idFile' column is also updated in both the 'movie' table and
-the 'files' table.
+This script changes the order of the movies by modifiying the ID ('idMovie')
+column in the 'movies' table to be in the same order as the creation-timestamps
+of the movie files themselves.  The 'idFile' column is also updated in both
+the 'movie' table and the 'files' table.
 
-In order to accomplish this, the first thing the script will do is find the largest 'idMovie'
-value in the 'movie' table.  If you have 100 movies, then this id will probably be 100. 
-All of the ID's in the 'movie' table will be shifted to be between 101 and 200. 
-The same is done for file ID's in the 'files' table.
+In order to accomplish this, the first thing the script will do is find the
+largest 'idMovie' value in the 'movie' table.  If you have 100 movies, then
+this id will probably be 100.  All of the ID's in the 'movie' table will
+be shifted to be between 101 and 200.  The same is done for file ID's
+('idFile') in the 'files' table.
 
-Each time the script is run, the ID's will be shifted higher.  This will leave a lot of holes
-at the "bottom" of the ID range, but this should not be a problem.  It doesn't look like
-XBMC uses the movie ID for anything other than sort order (as far as I know.)  However,
-because of this, I wouldn't recommend running this script very often.  You should
+Each time the script is run, the ID's will be shifted higher.  This will
+leave a lot of holes at the "bottom" of the ID range, but this should not
+cause problems.  It doesn't look like XBMC uses the movie ID for
+anything other than sort order (as far as I know.)  However, because
+of this, I wouldn't recommend running this script very often.  You should
 only need to run it after you reset your Library.
 
 Caveats:
 --------
-I have only done limited testing on this script.  It was tested on Windows 7, XBMC 10.0,
-and all of my movies were stored remotely via SMB.  It should work for local movies too,
-but other protocols may not work.  
+I have only done limited testing on this script.  It was tested on Windows 7,
+XBMC 10.0, and all of my movies were stored remotely via SMB.  It should
+work for local movies too, but other protocols may not work.  
 
 Known Bugs
 ----------
--- default.py
+++ default.py
@@ -18,18 +18,20 @@ class Main:
         max_file_id  = self.get_max_file_id()
         movie_list   = self.get_movies()
 
-        # iterate through each video, sorting by ctime, then update the video's id in the database
+        # iterate through each video, sorting by ctime, then update
+	# the video's id in the database
         i = 0
         for movie in sorted(movie_list, key=itemgetter(0)):
-            (ctime, old_idMovie, old_idFile, path) = (movie[0], movie[1], movie[2], movie[3])
+            (ctime, old_idMovie, old_idFile, fullFilePath) = (movie[0], movie[1], movie[2], movie[3])
             i += 1
             new_idMovie = max_movie_id + i
             new_idFile  = max_file_id + i
             if self.debug:
-                print "ctime: %d old_idMovie: %d new_idMovie: %d old_idFile: %d new_idFile: %d path: %s" % (ctime, old_idMovie, new_idMovie, old_idFile, new_idFile, path)
+                xbmc.log("ctime: %d old_idMovie: %d new_idMovie: %d old_idFile: %d new_idFile: %d file: %s" %
+                         (ctime, old_idMovie, new_idMovie, old_idFile, new_idFile, fullFilePath))
             self.update_movie_and_file_id(old_idMovie, new_idMovie, old_idFile, new_idFile)
         
-        xbmc.log('script.fixmoviesdateadded addon run complete')
+        xbmc.log('script.sortmoviesbyfiledate addon run complete')
 
     def get_max_movie_id(self):
         get_maxid_sql = "select max(idMovie) from movieview"
@@ -42,21 +44,21 @@ class Main:
         return int((re.findall( "<field>(.+?)</field>", sql_result, re.DOTALL ))[0])
 
     def get_movies(self):
-        """ fetch the idMove, idFile, and strPath of all movies in the video library,
-            then stat each strPath to get the CTIME (creation time).
-            Returns a list of tuples containing
-              (ctime, idMovie, idFile, path)
+        """ fetch the idMovie, idFile, and path+filename of all movies in
+            the Movie Library, then stat each file to get the CTIME
+            (creation time).  Returns a list of tuples containing
+              (ctime, idMovie, idFile, filename)
             eg:
             
-            [ (12312312312, 1, 5, "/Movies/Gladiator (2000)"),
-              (12423234223, 2, 4, "smb://user:pass@server/Movies/Something (2010)") ]
+            [ (12312312312, 1, 5, "c:/Movies/Gladiator (2000)/gladiator.mkv"),
+              (12423234223, 2, 4, "smb://user:pass@server/Movies/Something (2010)/something.avi") ]
         """
         movie_list = []
         xbmc.executehttpapi( "SetResponseFormat()" )
         xbmc.executehttpapi( "SetResponseFormat(OpenRecord,%s)" % ( "<record>", ) )
         xbmc.executehttpapi( "SetResponseFormat(CloseRecord,%s)" % ( "</record>", ) )
 
-        movies_sql = "select idMovie,idFile,strPath from movieview"
+        movies_sql = "select idMovie,idFile,strPath,strFileName from movieview"
         sql_result = xbmc.executehttpapi( "QueryVideoDatabase(%s)" % quote_plus( movies_sql ), )
         records = re.findall( "<record>(.+?)</record>", sql_result, re.DOTALL )
         for record in records:
@@ -64,14 +66,22 @@ class Main:
             idMovie = int(fields[0])
             idFile  = int(fields[1])
             strPath = xbmc.makeLegalFilename(fields[2])
-            ctime = os.stat(strPath)[ST_CTIME]
-            movie_list.append( (ctime, idMovie, idFile, strPath) )
+            strFileName = xbmc.makeLegalFilename(fields[3])
+            fullFilePath = xbmc.makeLegalFilename(os.path.join(strPath, strFileName))
+	    try:
+            	ctime = os.stat(fullFilePath)[ST_CTIME]
+            	movie_list.append( (ctime, idMovie, idFile, fullFilePath) )
+	    except OSError, e:
+	        xbmc.log("OSerror: %s, file: %s" % (e.strerror, e.filename))
         return movie_list
 
     def update_movie_and_file_id(self, old_idMovie, new_idMovie, old_idFile, new_idFile):
-        update_sql = "update movie set idMovie=%d, idFile=%d where idMovie=%d" % (new_idMovie, new_idFile, old_idMovie)
+        update_sql = ("update movie set idMovie=%d, idFile=%d where idMovie=%d" %
+                     (new_idMovie, new_idFile, old_idMovie))
         xbmc.executehttpapi( "ExecVideoDatabase(%s)" % quote_plus( update_sql ), )
-        update_sql = "update files set idFile=%d where idFile=%d" % (new_idFile, old_idFile)
+        
+        update_sql = ("update files set idFile=%d where idFile=%d" %
+                     (new_idFile, old_idFile))
         xbmc.executehttpapi( "ExecVideoDatabase(%s)" % quote_plus( update_sql ), )
 
         
